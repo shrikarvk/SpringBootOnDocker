8392d26edfacecbdb0f95a30d212bd8ba59820e0
Shrikar Kalagi <shrikarvk@gmail.com>
2019-03-06 13:18:37 +0530

    Initial commit of spring boot application on Docker

---
+++ src/main/java/helloDockerWorkd/Application.java
@@ -0,0 +1,26 @@
+/*
+Author: Shrikar Kalagi
+Date: 03/06/2019 
+*/ 
+
+package helloDockerWorkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
